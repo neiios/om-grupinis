f0473709760b02772e08dc1e8e87fc653a42d87d
Igor <egor@sgf.lt>
2023-05-26 16:15:28 +0300

    Add objective functions

--- main.py
+++ main.py
@@ -1,5 +1,50 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# https://www.sfu.ca/~ssurjano/grlee12.html
+def gramacy_lee(x: float):
+    return math.sin(10 * math.pi * x) / 2 * x + (x - 1) ** 4
+
+
+def plot_gramacy_lee():
+    x = np.linspace(0, math.pi, 1000)
+    y = np.array([gramacy_lee(val) for val in x])
+    plt.plot(x, y)
+    plt.show()
+
+
+# https://www.sfu.ca/~ssurjano/camel6.html
+def six_hump_camel(x1: float, x2: float):
+    part_one = (4 - 2.1 * x1**2 + (x1**4 / 3)) * x1**2
+    part_two = x1 * x2
+    part_three = (-4 + 4 * x2**2) * x2**2
+    return part_one + part_two + part_three
+
+
+def plot_six_hump_camel():
+    x1 = np.linspace(-2, 2, 100)
+    x2 = np.linspace(-1, 1, 100)
+
+    X1, X2 = np.meshgrid(x1, x2)
+    Z = six_hump_camel(X1, X2)
+
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot_surface(X1, X2, Z, cmap="viridis")
+
+    ax.set_xlabel("X1")
+    ax.set_ylabel("X2")
+    ax.set_zlabel("Z")
+    ax.set_title("3D Plot of the Six-Hump Camel Function")
+
+    plt.show()
+
+
 def main():
-    print("algo")
+    plot_gramacy_lee()
+    plot_six_hump_camel()
 
 
 if __name__ == "__main__":
