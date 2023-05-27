4aaa009f99ffaf068d5bff77e87444ef84ec5b33
tmneth <80415416+tmneth@users.noreply.github.com>
2023-05-27 21:42:05 +0300

    Remove unused limit logic

--- plot.py
+++ plot.py
@@ -1,5 +1,5 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Gramacy Lee version with numpy functions
@@ -8,9 +8,9 @@ def gramacy_lee(x: float) -> float:
 
 
 def six_hump_camel(x1: float, x2: float) -> float:
-    part_one = (4 - 2.1 * x1**2 + (x1**4 / 3)) * x1**2
+    part_one = (4 - 2.1 * x1 ** 2 + (x1 ** 4 / 3)) * x1 ** 2
     part_two = x1 * x2
-    part_three = (-4 + 4 * x2**2) * x2**2
+    part_three = (-4 + 4 * x2 ** 2) * x2 ** 2
     return part_one + part_two + part_three
 
 
@@ -23,40 +23,32 @@ def configurePlot(ax):
     ax.xaxis.get_major_ticks()[0].label1.set_visible(False)
 
 
-def drawGramacyLee(points, limit=True):
+def drawGramacyLee(points):
     points = [point[0] for point in points]
     fig, ax = plt.subplots()
 
-    if limit is False:
-        x = np.array(points)
-        y = f(x)
-        sizes = np.random.uniform(15, 100, len(points))
-        colors = np.random.uniform(30, 86, len(points))
-
-        ax.scatter(x, y, zorder=2, s=sizes, c=colors, vmin=0, vmax=100)
-    else:
-        indices = [0, 9, 19, 49, len(points) - 1]
-        x = np.array([points[i] for i in indices])
-        y = gramacy_lee(x)
-        for i, point in enumerate(zip(x, y)):
-            if i == len(indices) - 1:
-                offset_x, offset_y = 5, 10
-                bbox_props = dict(
-                    boxstyle="round,pad=0.3", fc="white", ec="black", lw=0.5
-                )
-                ax.scatter(point[0], point[1], color="red", zorder=3)
-            else:
-                offset_x, offset_y = 5, 10
-                bbox_props = dict(
-                    boxstyle="round,pad=0.3", fc="white", ec="black", lw=0.5
-                )
-            ax.annotate(
-                indices[i] + 1,
-                (point[0], point[1]),
-                xytext=(offset_x, offset_y),
-                textcoords="offset points",
-                bbox=bbox_props,
+    indices = [0, 9, 19, 49, len(points) - 1]
+    x = np.array([points[i] for i in indices])
+    y = gramacy_lee(x)
+    for i, point in enumerate(zip(x, y)):
+        if i == len(indices) - 1:
+            offset_x, offset_y = 5, 10
+            bbox_props = dict(
+                boxstyle="round,pad=0.3", fc="white", ec="black", lw=0.5
             )
+            ax.scatter(point[0], point[1], color="red", zorder=3)
+        else:
+            offset_x, offset_y = 5, 10
+            bbox_props = dict(
+                boxstyle="round,pad=0.3", fc="white", ec="black", lw=0.5
+            )
+        ax.annotate(
+            indices[i] + 1,
+            (point[0], point[1]),
+            xytext=(offset_x, offset_y),
+            textcoords="offset points",
+            bbox=bbox_props,
+        )
         ax.scatter(x[:-1], y[:-1], zorder=2)
 
     configurePlot(ax)
@@ -64,10 +56,8 @@ def drawGramacyLee(points, limit=True):
     x = np.linspace(-1, 2.5, 10000)
     y = gramacy_lee(x)
     plt.plot(x, y, zorder=1)
-    # plt.grid(alpha=.6, linestyle='--')
     plt.xlim((0.5, 2.5))
     plt.ylim((-1, 5))
-    # plt.title("Gramacy & Lee (2012) funkcijos minimizavimas", y=1.04)
     plt.show()
 
 
